54121c09b01d0fc58b24a6015f9517f7f8d5845b
hmarcuzzo <henrique.souza.m06@gmail.com>
2023-10-21 16:52:14 +0100

    Updated setup.py file and refactored project structure
    This commit updates the project setup.py file to include relevant project details such as the project's name, version, description, and author. Additionally, requirements are now fetched from requirements.txt file, improving project package management. The project structure was also refactored, renaming and restructuring directories for better organization and clarity. This prepares the project for distribution and deployment.

--- core/setup.py
+++ core/setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    lines = f.read().splitlines()
+    requirements = lines[1:] if lines[0] == "-i https://pypi.org/simple" else lines
+
 setup(
-    name="core",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[],
+    name="nutri_app_core",
+    version="0.0.1",
+    description="Core of NutriApp",
+    author="Henrique Marcuzzo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="nutri_app_core"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    package_dir={"": "nutri_app_core"},
+    install_requires=requirements,
 )
